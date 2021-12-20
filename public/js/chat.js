77b06f826967ac1eaf0268d624743dc0819f6eb0
rstephen2907 <rstephen2000@gmail.com>
2021-12-20 19:21:50 +0530

    event acknowledgement added

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,12 @@ socket.on('message', (msg) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
     const message = e.target.elements.message.value;
-    socket.emit('sendMessage', message);
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('The message is Delivered!');
+    });
 });
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -25,6 +30,11 @@ document.querySelector('#send-location').addEventListener('click', () => {
         return alert('Geolocation is not supported by your browser');
     }
     navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared successfully!');
+        });
     });
 })
