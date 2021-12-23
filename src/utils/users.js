f26007f08e66d14e32ad4159a2a9c0d06c12f697
rstephen2907 <rstephen2000@gmail.com>
2021-12-23 21:02:48 +0530

    storing users

---
+++ src/utils/users.js
@@ -0,0 +1,55 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, userName, room }) => {
+    // Clean the data
+    userName = userName.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    // Validate the data
+    if (!userName || !room) {
+        return {
+            error: 'Username and room are required!!!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => user.room === room && user.userName === userName);
+
+    // Validate userName
+    if (existingUser) {
+        return {
+            error: 'Username already in use!!'
+        }
+    }
+
+    // Store user
+    const user = { id, userName, room }
+    users.push(user);
+    return { user };
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id);
+
+    if (index != -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+const getUser = (id) => {
+    return users.find(user => user.id === id);
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase();
+    return users.filter(user => user.room === room);
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
